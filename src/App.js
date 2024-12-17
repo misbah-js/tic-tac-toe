26d9af76266cdbd06750ac1450f0a7dcd8da9308
misbah-js <misbah.js@tafsol.com>
2024-12-17 17:30:46 +0500

    lifitng state up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import Square from './Square';
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
-        <Square value={1}/>
-        <Square />
-        <Square />
+      <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
