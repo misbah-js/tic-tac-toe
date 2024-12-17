262196f3d0e17373f4c43d8cce2ebb55eff7ab91
misbah-js <misbah.js@tafsol.com>
2024-12-17 17:19:25 +0500

    using state

--- src/Square.js
+++ src/Square.js
@@ -1,6 +1,8 @@
-export default function Square({ value }) {
+import { useState } from "react";
+export default function Square() {
+    const [value, setValue] = useState(null);
     function handleClick() {
-      console.log('clicked!');
+        setValue("X")
     }
   
     return (
